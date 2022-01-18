3a12e4f4bbd2c047f2cdef15f9bc634b7f74e297
Lucas Augusto <nutkaugusto@gmail.com>
2022-01-18 20:34:57 -0300

    proto init

--- server.py
+++ server.py
@@ -2,22 +2,27 @@ from concurrent import futures
 
 import grpc
 
-import hello_pb2, hello_pb2_grpc
+import key_value_pb2, key_value_pb2_grpc
 
-class DoStuff(hello_pb2_grpc.DoStuffServicer):
+class KeyValueService(key_value_pb2_grpc.StorageServicer):
 
-    def say_hello(self, request, context):
-        print("Grpc server say_hello, pid = ", str(request.pid))
-        return hello_pb2.HelloReply(retval='Hello, %s' % str(request.pid))
+    key_value_list = []
+
+    def insert(self, request, context):
+        key, value = request
+
+        find = next((i for i, kv in enumerate(key_value_list) if kv[0] == key), None)
+
+        return key_value_pb2.InsertReply(reply=reply)
 
     
-    def say_hello_again(self, request, context):
+    def get(self, request, context):
         print("Grpc server say_hello_again, pid = ", str(request.pid))
         return hello_pb2.HelloReply(retval='Hello again, %s' % str(request.pid))
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
-    hello_pb2_grpc.add_DoStuffServicer_to_server(DoStuff(), server)
+    key_value_pb2_grpc.add_StorageServicer_to_server(KeyValueService(), server)
     server.add_insecure_port('localhost:8888')
     server.start()
     server.wait_for_termination()
